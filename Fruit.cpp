e6f271ea2651e854ba988493e29f65d9ca3f1158
noora084 <noura064@gmail.com>
2025-05-15 13:38:12 +0330

    new functions

--- Fruit.cpp
+++ Fruit.cpp
@@ -1,10 +1,32 @@
 #include<iostream>
-#include<iostream>
-#include "fruit.hpp"
+#include <string>
+#include <algorithm>
+#include "Fruit.hpp"
 using namespace std;
 
-Fruit::Fruit (int p , int a  , string me = "kg") : Item (p,a) , measurment(me){ 
-    cout<<" price ; "<< p << "available "<< a << me<<endl;
+std::vector<std::string> Fruit::names; 
+
+Fruit::Fruit (std::string n , int p , int a , string me) : measurment(me){
+    if (a <= 0 || p < 0 )
+        throw invalid_argument("an item can not be negative!!");
+    else{
+        available = a ;
+        price = p ; 
+        name = n ; 
+        names.push_back(n) ;
+        sort(names.begin() , names.end()) ; 
+    }
 }
 
-
+string Fruit::getInfo()const{
+    return get_name() + " price: " + to_string(get_price()) + ",available: " + to_string(get_available()) + " " + measurment ; 
+}
+std::string Fruit::get_name()const{
+    return name ; 
+}
+int Fruit::get_price()const{
+    return price ; 
+}
+int Fruit::get_available()const{
+    return available ; 
+}
