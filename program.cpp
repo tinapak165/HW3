909a31b21fcd413dc8e2a827515df62940515702
Noora Panahi <nourap064@gmail.com>
2025-05-21 03:45:52 -0700

    deleted showingBasket

--- program.cpp
+++ program.cpp
@@ -20,67 +20,7 @@ string Program::showItems(){
     
     return result ; 
 }
-// void Program::showingBasket(const std::string &item, int tedad) {
-//     if(tedad <=0) return ; //do nothing
-//     bool found ; 
-//     for(auto & ba : basket){
-//         if(item == ba.second){
-//             if(ba.first == 0){
-//                 found = true ;
-//                 break ; 
-//             }
-//             else if(tedad > ba.first){
-//                 found = true ;
-//                 break;
-//             }
-//             else{
-//             ba.first += tedad;
-//             found = true ;      
-//             break;           
-//             }
-//         }
 
-//     }
-
-//     if(!found){
-//         basket.push_back({tedad, item});
-//     }
-
-//     for(const auto& bas : basket) 
-//         cout << bas.first << " " << bas.second << endl;
-// }
-
-
-void Program::showingBasket(const std::string &item, int tedad) {
-    if(tedad <= 0) return;
-
-    bool found = false;
-    
-    for(auto &ba : basket) {
-        if(item == ba.second && ba.first == 0) {
-            cout << "Item has sold out completely" << endl;
-            break;
-        }
-    }
-
-    for(auto &ba : basket) {
-        if(item == ba.second) {
-            ba.first += tedad;
-            found = true;
-        
-        }
-    }
-
-    if(!found) {
-        basket.push_back({tedad, item});
-    }
-
-    for(const auto& bas : basket) {
-        if(bas.first > 0) {
-            cout << bas.first << " " << bas.second << " " << endl;
-        }
-    }
-}
 void Program::Run(){
     try
     {
@@ -126,11 +66,11 @@ void Program::Run(){
                 
                 cout << "how many " << targetItem << " do you want to buy? " ; 
                 cin >> TedadTarget ; 
+                if(TedadTarget < 0)
+                    continue;
+
                 Program p ; 
                 p.Buying(targetItem,  TedadTarget) ;
-    
-                cout<< " your basket :";
-                p.showingBasket(targetItem,TedadTarget) ; 
 
                 //cout << "your bank account: " ; //showaccount()
             }
