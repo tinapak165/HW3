687dcd4c35e775421c51b518d7fd5d8b0c596ffa
Noora Panahi <nourap064@gmail.com>
2025-05-21 07:06:04 -0700

    changed buy function

--- Seasoning.cpp
+++ Seasoning.cpp
@@ -30,8 +30,11 @@ int Seasoning::get_available()const{
 void Seasoning::buy(int b){
     if ( !is_available())
         throw runtime_error("item not available") ; 
-    if(b > available)
+    else if(b > available)
         throw runtime_error("not enough item") ; 
+    else
+        cout << "you bought " << b << " " << get_name() << endl ; 
+
     
     set_available(available - b) ; 
 
