0228d98f81621d9714bfd6e7a1d4c9c30311717e
Noora Panahi <nourap064@gmail.com>
2025-05-22 12:56:28 -0700

    operator << overloaded

--- currency.hpp
+++ currency.hpp
@@ -1,7 +1,10 @@
 #ifndef CURRENCY_HPP
 #define CURRENCY_HPP
+#include <iostream>
+
 // این فایل فقط برای پیاده سازی کلاس های مشتق شدست
 class Currency {
+    friend std::ostream& operator<<(std::ostream & output , Currency& c);
     public:
         virtual double to_usd() const = 0;
         virtual ~Currency() = default;
