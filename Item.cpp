1940c7dbaefc4d54766ad0dee6d386d1e1f23de3
Noora Panahi <nourap064@gmail.com>
2025-05-21 22:59:51 -0700

    fixed constructor

--- Item.cpp
+++ Item.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-Item::Item(std::string n , int a , int p){
+Item::Item(std::string n , int p , int a){
 
     if (a <= 0 || p < 0 )
         throw invalid_argument(" cant be negative");
@@ -36,6 +36,5 @@ string Item::get_Name() const {
     
     return name;
 }
-Item::Item() : available(0) , price(0) , name(" ") {}
 
 
