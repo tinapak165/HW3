e19f8c3707680159b3cd1a165e23df62d406abed
Noora Panahi <nourap064@gmail.com>
2025-05-22 12:15:30 -0700

    defined override and changed functions

--- Fruit.hpp
+++ Fruit.hpp
@@ -10,14 +10,13 @@ class Fruit : public Item {
     std::string name ; 
 
     public:
-    Fruit( std::string name ,int p , int a ,std::string me = "kg");
-    void buy(int b) override; 
-    std::string getInfo() override ;
-    std::string get_name() const; 
-    int get_price() const; 
-    int get_available()const ; 
-    void set_available(int a) ; 
-    bool is_available() ; 
-    
+        Fruit( std::string name ,int p , int a ,std::string me = "kg");
+        void buy(int b) override; 
+        std::string getInfo() override ;
+        std::string get_Name() const override ;  
+        int getPrice() const override; 
+        int get_available()const override ; 
+        void set_available(int a) override; 
+        bool is_available() const override; 
 
 };
